2e18a368856c4bd1f7b8197b6e47d690327d1983
Luis Cadillo <luiscaf3r@gmail.com>
2025-01-27 15:10:33 -0500

    add cf compatible load function

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,12 +1,7 @@
-import * as fs from "node:fs/promises";
-import { join } from "node:path";
-import type { PageLoad } from "./$types";
+import demoMd from '$lib/demo.md?raw';
 
-export const load: PageLoad = async () => {
-    let path = join(process.cwd(), "src/demo.md");
-    const md = await fs.readFile(path, "utf-8");
-
+export const load = async () => {
     return {
-        md
+        md: demoMd
     };
-}
+};
